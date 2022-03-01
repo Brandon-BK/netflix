c065080c7ef0a3391fda05021cb1c3f20d8b8ccf
brandon <kisibubrandon5@gmail.com>
2022-03-01 12:43:41 +0200

    download section

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,11 @@
+import Download from '../components/Download'
 import NavBanner from '../components/NavBanner'
 
 export default function Home() {
   return (
     <>
       <NavBanner />
+      <Download />
     </>
   )
 }
