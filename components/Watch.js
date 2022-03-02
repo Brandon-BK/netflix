29630626add86a9fe44fe8546feb48897e2ed9e3
brandon <kisibubrandon5@gmail.com>
2022-03-02 08:11:56 +0200

    watch section

---
+++ components/Watch.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Watch = () => {
+  return (
+    <div style={{height:300, width:'100%',borderTop:'5px solid grey',borderBottom:'5px solid grey',background:'black',color:'white',padding:'50px 45px'}}>
+        <h1 style={{fontSize:50}}>Watch everywhere.</h1>
+        <p style={{fontSize:28}}>
+        Stream unlimited movies and TV shows on <br /> your phone, tablet, laptop, and TV.
+            </p>
+    </div>
+  )
+}
+
+export default Watch
